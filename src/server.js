cd79bfe112af9696e559878854ff5b1f3191b3e4
Nihat <senius.33@gmail.com>
2022-07-15 02:11:42 +0300

    update mongo

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const http = require("http");
 
+const app = require("./app");
+
 require("dotenv").config();
 
-const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3001;
 
 async function startServer() {
-  await mongoConnect();
+  await mongoConnect(process.env.MONGO_URL);
 
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
