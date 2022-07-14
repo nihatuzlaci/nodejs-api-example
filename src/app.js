a3c11fa613385d14f00205c11e53c0555a9fac15
Nihat <senius.33@gmail.com>
2022-07-15 02:11:33 +0300

    add base routes

--- src/app.js
+++ src/app.js
@@ -4,9 +4,21 @@ const morgan = require("morgan");
 
 const app = express();
 
+const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes");
+const likeRoutes = require("./routes/likeRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const friendshipRoutes = require("./routes/friendshipRoutes");
+
 app.use(cors());
 app.use(morgan("combined"));
 
 app.use(express.json());
+
+app.use("/user", userRoutes);
+app.use("/post", postRoutes);
+app.use("/like", likeRoutes);
+app.use("/comment", commentRoutes);
+app.use("/friendship", friendshipRoutes);
 
 module.exports = app;
