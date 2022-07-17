3f188c774be9b1b4059e1f441308cc81c6119c1f
Nihat <senius.33@gmail.com>
2022-07-17 23:53:17 +0300

    fix toggleLike method

---
+++ src/controllers/likeController.js
@@ -0,0 +1,56 @@
+const Like = require("../models/Like");
+
+const toggleLikePost = async (req, res) => {
+  try {
+    const { postId, userId } = req.params;
+
+    const post = await Like.findOne({ post: postId, user: userId });
+    if (post) {
+      post.user = undefined;
+      await post.save();
+      return res.status(200).json({
+        success: true,
+        message: "Like removed",
+      });
+    }
+
+    const like = await Like.findOneAndUpdate(
+      { post: postId },
+      { post: postId, user: userId },
+      { upsert: true, new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: like,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const getLikesByPostId = async (req, res) => {
+  try {
+    const likes = await Like.find({ post: req.params.postId }).populate([
+      "post",
+      "user",
+    ]);
+    res.status(200).json({
+      success: true,
+      data: likes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Interval server error",
+    });
+  }
+};
+
+module.exports = {
+  toggleLikePost,
+  getLikesByPostId,
+};
